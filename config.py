e9829abaad375b315d99b5fb02411f3429057a01
rajpomane07 <119722963+rajpomane07@users.noreply.github.com>
2022-12-03 20:05:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "25603418")) #optional
+API_HASH = getenv("API_HASH", "53103f2d622711bc2f10b59763c151d7") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC")
-ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5447310526").split()))
+OWNER_ID = int(getenv("OWNER_ID", "@BATTERY_ABOUT_TO_DAI_ERROR"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://userbot:userbot@cluster0.q5lvfgg.mongodb.net/?retryWrites=true&w=majority"
+BOT_TOKEN = getenv("BOT_TOKEN", "5963859529:AAH0lImg0U6iBb8q3b4d2jl7HYQgFX-J0yY")
+ALIVE_PIC = getenv("ALIVE_PIC", "https://graph.org/file/dee44cef6db27983dd8ab.jpg"
+ALIVE_TEXT = getenv("ALIVE_TEXT", "𝐓ᴇʀᴇ #🅺ᴀᴛɪʟ 𝐍ᴀᴢᴀʀ 𝐀ᴜʀ 𝐘ᴇ 🅰ᴅᴀᴀ||🅶ᴀɴᴅ 𝐌ᴇ 🅳ᴀᴀʟ 𝐀ᴜʀ 𝐌ᴀ #🅲ʜᴜᴅᴀ"
+PM_LOGGER = getenv("PM_LOGGER", "-1001693276872"
+LOG_GROUP = getenv("LOG_GROUP", "-1001693276872"
 
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BACVif7kFk86KpO9bIRkJcZrGTBPkZBg7T5CC7hqhZvVWGiHT9aHN69NBLWK69d7T0IQM0MdmVbB1TtVhVifKkT8CVmOejo517ClKZtxsblcloOLda8yxYuCl78oMUMvf1nBA5l6qQN5OV1jofEgYhPRP1QDpl9G2DP8-yOhwISTIbeyfkU0_bOb5VUguF_nzaXSu8Ko4WlISMdsnC-hfGGHNUYTmAzE-E7EHS7JfR1WwI_q6pIGMstjOsaXUFcpy6EdPO_c5iPQ0MqbBk-BZuyceuEH0edesgMVlUN25dTbTsi-YMbRpt6dIu33bzd38xmk8SZvcW7tzi-6NkDkOBFuAAAAAUSvXL4A")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
